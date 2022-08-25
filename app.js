ab29e0aca9a430a366adf8e97fb644b81edd2301
Lucas <lucasfrmr@gmail.com>
2022-08-25 16:32:21 -0500

    Replaced puppeteer with axios.

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express')
 const path = require('path')
 const pretty = require("pretty");
+const axios = require('axios');
 
 //Scrapper
-const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
 const res = require('express/lib/response');
 
 // //Scrapper
 
 
+  // axios.get('https://www.amazon.com/dp/B09YRJLNW1').then((response) => {
+  //     const $ = cheerio.load(response.data)
+  //     return $('.prodDetAttrValue').text()
+  // })
+  // .catch((error) => {
+  //     console.log(error)
+  // })
 
 
 const app = express()
@@ -51,45 +58,46 @@ app.get('/2bcode', (req, res) => {
     title: "2Bcode"
   })
 })
-// const pdata = element.text();
 
-  // (async () => {
-  //   const browser = await puppeteer.launch({
-  //       headless: true,
-  //   });
-  //   const page = await browser.newPage();
-  //   await page.goto("https://amazon-asin.com/asincheck/?product_id=B09YRJLNW1");
-
-  //   await page.screenshot({ path: "image1.png"});
-
-  //   const pageData= await page.evaluate(() => {
-  //       return {
-  //           html: document.documentElement.innerHTML,
-  //           width: document.documentElement.clientWidth,
-  //           height: document.documentElement.clientHeight,
-  //       };
-
-  //   });
-    
-  //   // console.log(pageData);
-
-  //   const $ = cheerio.load(pageData.html);
-  //   const element = $(".ng-binding")
-  //   console.log(element.text());
-  //   // pdata = element.text();
-  //   return element.text();
-  //   await browser.close();
-  // })();
-
-
-
-app.get('/bindornaw',(req, res) => {
+app.get('/bindornaw', (req, res) => {
   res.render('bindornaw', {
-    title: "Bind or naw",
-    data: pdata()
+    title: "bindornaw"
   })
-  console.log(pdata);
 })
+
+
+
+app.get('/bindornaw_2',(req, res) => {
+  axios.get('https://www.amazon.com/dp/B09YRJLNW1', {headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:103.0) Gecko/20100101 Firefox/103.0'}}).then((response) => {
+      const $ = cheerio.load(response.data)
+
+      console.log($('.prodDetAttrValue').text().split(" "))
+      res.render('bindornaw_2', {
+        title: "Bind or naw v2",
+        data: $('.prodDetAttrValue').text().split(" ")
+        
+    })
+  })
+  .catch(function (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', error.message);
+    }
+  })
+})
+  // console.log(pdata);
+
 
 
 app.listen(app.get('port'), () => {
