c77a8d6ba11d15e7d47a2f755f443a8062138c24
Lucas <lucasfrmr@gmail.com>
2022-09-15 17:08:43 -0500

    small updates

---
+++ LF.md
@@ -0,0 +1,20 @@
+My Site
+- click counter- 25 cents a click total bill on page
+links
+    -youtube
+    -spotify
+    -insta
+    -steam
+    -discord
+
+
+
+
+
+    - ipinfo collector
+    - use ip data to insert message
+    - sketches
+    - videos
+    - scoreboard for highest score on game
+    - footer font rems to be dynamic
+    - rick roll
--- app.js
+++ app.js
@@ -2,22 +2,9 @@ const express = require('express')
 const path = require('path')
 const pretty = require("pretty");
 const axios = require('axios');
-
-//Scrapper
 const cheerio = require("cheerio");
 const res = require('express/lib/response');
-
-// //Scrapper
-
-
-  // axios.get('https://www.amazon.com/dp/B09YRJLNW1').then((response) => {
-  //     const $ = cheerio.load(response.data)
-  //     return $('.prodDetAttrValue').text()
-  // })
-  // .catch((error) => {
-  //     console.log(error)
-  // })
-
+const bodyParser = require("body-parser");
 
 const app = express()
 
@@ -30,9 +17,19 @@ app.use(express.json())
 app.locals.pretty = true
 app.disable('x-powered-by')
 
+// create application/x-www-form-urlencoded parser
+// var urlencodedParser = bodyParser.urlencoded({ extended: false })
+// create application/json parser
+// var jsonParser = bodyParser.json()
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
+  const ipAddress = req.socket.remoteAddress;
   res.render('index', {
-    title:'LF'
+    title:'LF',
+    userIp: ipAddress
   })
 })
 
@@ -59,19 +56,68 @@ app.get('/2bcode', (req, res) => {
   })
 })
 
+app.get('/bcode_2', (req, res) => {
+  res.render('bcode_2', {
+    title: "Bcode_2"
+  })
+})
+
 app.get('/bindornaw', (req, res) => {
   res.render('bindornaw', {
     title: "bindornaw"
   })
 })
 
+app.get('/inputParser', (req, res) => {
+  res.render('inputParser', {
+    title: "inputParser"
+  })
+})
+
+// https://codeforgeek.com/handle-get-post-request-express-4/
+
+app.post('/login', function (req, res) {
+  var asin = req.body.name;
+  axios.get('https://www.amazon.com/dp/' + asin, {headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:103.0) Gecko/20100101 Firefox/103.0'}}).then((response) => {
+      const $ = cheerio.load(response.data)
+
+      console.log($('.prodDetAttrValue').text().split(" "))
+      res.render('bindornaw_2', {
+        title: "Bind or naw v2",
+        data: $('.prodDetAttrValue').text().split(" ")
+        
+    })
+  })
+  .catch(function (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', error.message);
+    }
+  })
+
+
+  
+  // res.send('welcome, ' + req.body.name)
+  console.log(asin);
+})
 
 
 app.get('/bindornaw_2',(req, res) => {
   axios.get('https://www.amazon.com/dp/B09YRJLNW1', {headers: {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:103.0) Gecko/20100101 Firefox/103.0'}}).then((response) => {
       const $ = cheerio.load(response.data)
 
-      console.log($('.prodDetAttrValue').text().split(" "))
+      // console.log($('.prodDetAttrValue').text().split(" "))
       res.render('bindornaw_2', {
         title: "Bind or naw v2",
         data: $('.prodDetAttrValue').text().split(" ")
